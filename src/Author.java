de6d2dbf171f08893284766afba4ee9e547e8a4f
vladk <kotolupenko.vlad@mail.ru>
2023-04-18 12:46:06 +0500

    дз

---
+++ src/Author.java
@@ -0,0 +1,34 @@
+import java.util.Objects;
+
+public class Author {
+    private     String name;
+
+
+    private  String surname;
+
+    public  Author( String name, String surname){
+        this.name=name;
+        this.surname=surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(name, author.name) && Objects.equals(surname, author.surname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+
+        Author Griboyedov = new Author(" Alexander  ", " Griboyedov ");
+        Author Starikov = new Author("Nikolay", " Starikov ");
+
+        Book WoeFromWit = new Book(" Woe From Wit ", Griboyedov, 1824);
+        Book Power = new Book("Power", Starikov, 2015);
+
+        System.out.println(" WoeFromWit:");
+        System.out.println(WoeFromWit.getName());
+        System.out.println(WoeFromWit.getAuthor().getName() + "  " + WoeFromWit.getAuthor().getSurname());
+        System.out.println(WoeFromWit.getYear());
+
+        System.out.println();
+
+        System.out.println(" Power:");
+        System.out.println(Power.getName());
+        System.out.println(Power.getAuthor().getName() + " " + Power.getAuthor().getSurname());
+        System.out.println(Power.getYear());
+
+        Power.setYear( 2020);
+        System.out.println("Год издания Power " + Power.getYear() );
+    }
+}
